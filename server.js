f6778f37a2dc5e8168d912b02179e6220108413c
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2024-09-22 20:53:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const shortid = require('shortid');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Atlas Connection URL (replace <db_password> with your password)
+const uri = 'mongodb+srv://hritikpawardev:pawar2700@bookkeeper.hv4oh.mongodb.net/?retryWrites=true&w=majority&appName=BookKeeper';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Database and Collection Name
+const dbName = 'TrimLink';
+const collectionName = 'urls';
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB Atlas');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+connectToDatabase();
+
+// Shorten URL API
+app.post('/shorten', async (req, res) => {
+    const { originalUrl } = req.body;
+
+    // Generate short URL using your custom domain
+    const shortUrl = `http://localhost:5000/${shortid.generate()}`;
+
+
+    try {
+        const database = client.db(dbName);
+        const urlsCollection = database.collection(collectionName);
+
+        // Insert the new shortened URL into the database
+        const result = await urlsCollection.insertOne({
+            originalUrl,
+            shortUrl,
+            date: new Date(),
+        });
+
+        res.json({ shortUrl });
+    } catch (error) {
+        console.error('Error inserting into database:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+// Redirect to the original URL
+app.get('/:shortUrl', async (req, res) => {
+    const { shortUrl } = req.params;
+
+    try {
+        const database = client.db(dbName);
+        const urlsCollection = database.collection(collectionName);
+
+        // Find the original URL corresponding to the short URL
+        const urlDoc = await urlsCollection.findOne({ shortUrl: `http://localhost:5000/${shortUrl}` });
+
+        if (urlDoc) {
+            res.redirect(urlDoc.originalUrl);
+        } else {
+            res.status(404).json('URL not found');
+        }
+    } catch (error) {
+        console.error('Error finding URL in database:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
